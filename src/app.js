e36619d50babce89b36a1e971677c81a726301d8
Lisa Marie Moreno <108356315+lisamarie245@users.noreply.github.com>
2024-11-14 20:20:10 +0000

    Excuse-Generator-By-Lisamarie245-FP91S

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // ---- excuses ---
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let indicewho = Math.floor(Math.random() * who.length);
+  let indiceaction = Math.floor(Math.random() * action.length);
+  let indicewhat = Math.floor(Math.random() * what.length);
+  let indicewhen = Math.floor(Math.random() * what.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[indicewho] +
+    " " +
+    action[indiceaction] +
+    " " +
+    what[indicewhat] +
+    " " +
+    when[indicewhen];
 };
